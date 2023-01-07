adb45b1ea6cc7aa8855082dae16010469e8a65e7
Bernard065 <bebenibernard@gmail.com>
2023-01-07 05:51:44 +0300

    add hex.js

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const colors = ['green', 'red', 'rgba(133, 122, 200)', '#f15025'];
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
 
-//reference elements
-const btn = document.querySelector('#btn');
-const col = document.querySelector('.color');
+btn.addEventListener("click", function () {
+  const randomNumber = getRandomNumber();
+  // console.log(randomNumber);
 
-//add eventlisteners
-btn.addEventListener('click', () => {
-    colors.map(color => {
-        document.body.style.backgroundColor = color;
-        col.textContent = color;
-    })
-}) 
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
